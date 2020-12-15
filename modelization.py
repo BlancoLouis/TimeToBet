3a1c63f1096ef7a7e7b71308ea3e6c459376399d
BlancoLouis <louis.blanco@hec.edu>
2020-12-15 11:07:58 +0100

    Initial commit

--- modelization.py
+++ modelization.py
@@ -240,7 +240,8 @@ def sep_by_team(dataset):
 
 def regs(datafile, minute, team):
     """Crée un modèle de régression logistique multiclasse,
-    entraîné sur un train set, testé sur un test set."""
+    entraîné sur un train set standardisé, testé sur un test 
+    set standardisé."""
     data = sep_by_time(sep_by_team(pd.read_csv(datafile)))
 
     X = data[team][minute - 20]
